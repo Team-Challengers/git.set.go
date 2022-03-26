58a5ac03f15c371d704588188e6ad068f2e3dee4
pratiknyk19 <102415102+pratiknyk19@users.noreply.github.com>
2022-03-26 17:36:59 +0530

    Update fix_me.c

--- fix_me.c
+++ fix_me.c
@@ -9,11 +9,12 @@ int main() {
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
-    sum = number1 - number2;      
+    sum = number1 + number2;      
 
     // Multiply number 1 and number 2
     multi = number1 * number1
     
     printf("%d + %d = %d", number1, number2, sum);
+    printf("%d * %d = %d", number1, number2, multi);
     return 0;
 }
