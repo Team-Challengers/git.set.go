d2af327f3350408a88f361c29d67703b26cb6834
Dembouza <102415233+Dembouza@users.noreply.github.com>
2022-03-26 17:49:17 +0530

    Update fix_me.py

--- fix_me.py
+++ fix_me.py
@@ -1,13 +1,13 @@
 
-print("Enter the two number")
+print("Enter the two numbers")
 # Input Two numbers
 a= int(input("Enter the first no: "))
 b= int(input("Enter the second no: "))
 # Divide two numbers
-division= a-b
+division= a/b
 
 # Square of first no.
-a = a+a
+a = a*a
 # Square of second no.
 b = b*b
 print("Division",division)
