5dc595b7ef9a84ba88e5919064fd568e8349ffe7
braib <102415109+braib@users.noreply.github.com>
2022-03-26 18:03:49 +0530

    Update fix_me.c

--- fix_me.c
+++ fix_me.c
@@ -3,19 +3,21 @@
 
 int main() {    
 
-    int number1, number2, sum, multi, substract;
+    int number1, number2, sum, multi, substract, div;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
     sum = number1 + number2;      
-
+    
     // Multiply number 1 and number 2
     multi = number1 * number2;
+    div = number1 / number2;
     substract = number1 - number2;
     printf("%d + %d = %d", number1, number2, sum);
     printf("%d - %d = %d", number1, number2, substract);
     printf("%d * %d = %d", number1, number2, multi);
+    printf("%d / %d = %d", number1, number2, div);
     return 0;
 }
