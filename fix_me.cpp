144a8aeed8038bcc121295314d283b75e662d0e9
LoboVivin <102316308+lobo-vivin-03@users.noreply.github.com>
2022-03-26 17:49:41 +0530

    Update fix_me.cpp

--- fix_me.cpp
+++ fix_me.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main() {
 
-  int first_number, second_number;
+  int first_number, second_number,sum;
 
   cout << "Enter two integers: ";
   cin >> first_number >> second_number;
 
   // sum of two numbers in stored in variable sumOfTwoNumbers
-   
+   sum = first_number + second_number;
 
   // prints sum 
-  cout <<"sum = "<< first_number + second_number;     
+  cout <<"sum = "<<sum;     
 
   return 0;
 }
