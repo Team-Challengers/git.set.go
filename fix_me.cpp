54076a94ff882a03f87007268126018ce14bd884
LoboVivin <102316308+lobo-vivin-03@users.noreply.github.com>
2022-03-26 17:51:02 +0530

    Update fix_me.cpp

--- fix_me.cpp
+++ fix_me.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-  int first_number, second_number,sum;
+  int first_number, second_number, sum;
 
   cout << "Enter two integers: ";
   cin >> first_number >> second_number;
